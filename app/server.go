8b2ceea82df49470b1bb2b623bad6df29a2f3c65
Gennady Badinov <gennady.badinov@gmail.com>
2024-08-03 12:44:00 +0300

    pass stage 2

--- app/server.go
+++ app/server.go
@@ -1,23 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
-	"os"
+)
+
+const (
+	network    = "tcp"
+	listenAddr = "0.0.0.0:4221"
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	l, err := net.Listen(network, listenAddr)
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+		log.Fatalf("failed to bind to port")
 	}
+	log.Printf("app started on %s", listenAddr)
 
-	_, err = l.Accept()
+	c, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		log.Fatalf("error accepting connection %v", err)
 	}
+	log.Printf("HTTP request accepted")
+
+	okResp := "HTTP/1.1 200 OK\r\n\r\n"
+	if _, err := c.Write([]byte(okResp)); err != nil {
+		log.Fatalf("error writing response %v", err)
+	}
+	log.Printf("HTTP response 200 OK sent")
 }
